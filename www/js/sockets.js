a0574d76ba42eab05235c0e5fb53e09009428b43
Benjamin Eskildsen <eskildsen.ben@gmail.com>
2023-11-20 21:11:51 -0500

    actually fix sockets with env

--- www/js/sockets.js
+++ www/js/sockets.js
@@ -1,6 +1,8 @@
 
 export const setupSocket = (dispatch) => {
-  const socket = io(window.location.hostname);
+  let hostname = window.location.hostname;
+  if (hostname == 'localhost') hostname += ':8000';
+  const socket = io(hostname);
   socket.on("action", dispatch);
   return socket;
 }
