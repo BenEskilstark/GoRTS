e5213d1d8816db33255359d12d8881164a104420
Vance Zuo <vancezuo@Vances-MacBook-Air.local>
2024-01-12 21:47:50 -0800

    Reset config

--- www/js/config.js
+++ www/js/config.js
@@ -1,23 +1,3 @@
-// export const config = {
-//   isRealtime: true,
-//   turnTime: 0, // ms
-//   boardSize: 30,
-//   colors: [
-//     'black', 'white', 'steelblue', 'lightgreen', 'pink',
-//     'orange', 'red', 'purple', 'blue', 'gray',
-//   ],
-
-//   startingMana: 25,
-//   maxMana: 50,
-//   manaRegenRate: 1000,
-
-//   aiNeighborWeight: 500,
-//   apm: 150,
-
-//   fallingTime: 3000, // ms that a piece spends falling before it's placed
-
-// };
-
 export const config = {
   isRealtime: true,
   turnTime: 0, // ms
@@ -29,11 +9,31 @@ export const config = {
 
   startingMana: 25,
   maxMana: 50,
-  manaRegenRate: 10,
+  manaRegenRate: 1000,
 
   aiNeighborWeight: 500,
-  apm: 1500,
+  apm: 150,
 
-  fallingTime: 30, // ms that a piece spends falling before it's placed
+  fallingTime: 3000, // ms that a piece spends falling before it's placed
 
 };
+
+// export const config = {
+//   isRealtime: true,
+//   turnTime: 0, // ms
+//   boardSize: 30,
+//   colors: [
+//     'black', 'white', 'steelblue', 'lightgreen', 'pink',
+//     'orange', 'red', 'purple', 'blue', 'gray',
+//   ],
+
+//   startingMana: 25,
+//   maxMana: 50,
+//   manaRegenRate: 10,
+
+//   aiNeighborWeight: 500,
+//   apm: 1500,
+
+//   fallingTime: 30, // ms that a piece spends falling before it's placed
+
+// };
